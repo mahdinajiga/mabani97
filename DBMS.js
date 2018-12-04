311b66715a9ce128cb34eaeb61644491074dd02c
MahdiNaji <mahdinajiga@yahoo.com>
2018-12-04 12:17:54 +0330

    12/4/2018 12:15 second major commit. servicing university database query on /uni and searching for submited users by bachelor id or degree or a word in biography. 6Hours work till now

--- DBMS.js
+++ DBMS.js
@@ -15,6 +15,8 @@ var submitedUserConfirmed = function () {
     /*
         BachId      :   arguments[0],
         CodeMelli   :   arguments[1],
+        Degrees     :   arguments[2],
+        DegsCount   :   arguments[3]
     */
     if(fs.existsSync(NodesDir+"/uniDatabase/"+arguments[0]))
     {
@@ -23,11 +25,33 @@ var submitedUserConfirmed = function () {
             var BachOnUniDatabase = JSON.parse(fs.readFileSync(NodesDir+"/uniDatabase/"+arguments[0]));
             if(BachOnUniDatabase.CodeMelli != arguments[1]) 
             {
+                console.log("code melli wrong");
                 return 0;
             }
             else
             {
-                return 1;
+                var Degs = arguments[2]; // user given degrees to check
+                var UniDegs = BachOnUniDatabase.Degs; // uni database degrees confirmed for user
+                var ResOfDegreeConfirm = 1; // user given data confirm flag
+                for(var i=0; i<arguments[3]; i++) // search all user given degrees
+                {
+                    var DegreeFound = 0; // assume given data is wrong
+                    for(var j=0; j<BachOnUniDatabase.DegsCount && DegreeFound==0; j++) // search all uniDatabase degrees confirmed for user
+                    {
+                        
+                        if(UniDegs[j].DegreeName == Degs[i].DegreeName && UniDegs[j].SalEtmam == Degs[i].SalEtmam)
+                        {
+                            console.log("deg found: "+ Degs[i].DegreeName + " on "+Degs[i].SalEtmam)
+                            DegreeFound = 1; // this degree is confirmed by university
+                        }
+                    }
+                    if(DegreeFound==0)
+                    {
+                        console.log("deg not found: "+ Degs[i].DegreeName + " on "+Degs[i].SalEtmam)
+                        ResOfDegreeConfirm = 0; // one of degrees are wrong (not confirmed by university)
+                    }
+                }
+                return ResOfDegreeConfirm;
             }
         }
         catch(ert)
@@ -49,6 +73,89 @@ var submitUser = function () {
    fs.writeFileSync(NodesDir+"/submiteds/"+arguments[0], JSON.stringify(arguments[1]));
 }
 
+var confirmedUserExists = function () {
+    if (fs.existsSync(NodesDir+"/uniDatabase/"+arguments[0])){
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+var confirmUser = function () {
+    /*
+        BachId      :   arguments[0],
+        Bach        :   arguments[1],
+    */
+   fs.writeFileSync(NodesDir+"/uniDatabase/"+arguments[0], JSON.stringify(arguments[1]));
+}
+
+var GetUserInfo = function () {
+    console.log(arguments[0]);
+    return JSON.parse(fs.readFileSync(NodesDir+"/submiteds/"+arguments[0]));
+}
+
+var SearchUserByBioWord = function () {
+    var QueryObj = arguments[0];
+    var ReturnObj = [];
+    var files = fs.readdirSync(NodesDir+"/submiteds/");
+    for(var i=0; i<files.length; i++)
+    {
+        var BachData = JSON.parse(fs.readFileSync(NodesDir+"/submiteds/"+files[i]));
+        var UserBio = BachData.Bio;
+        var UserDeg = BachData.Degs;
+        if(QueryObj.word == "") // query with degree only
+        {
+            if(QueryObj.DegreeName == "0")
+            {
+                ReturnObj.push(BachData);
+            }
+            else
+            {
+                var UserFound = 0;
+                for(var j=0; j<UserDeg.length && UserFound==0; j++)
+                {
+                    if(UserDeg[j].DegreeName == QueryObj.DegreeName && UserDeg[j].SalEtmam == QueryObj.SalEtmam)
+                    {
+                        UserFound = 1;
+                    }
+                }
+                if(UserFound==1)
+                {
+                    ReturnObj.push(BachData);
+                }
+            }
+        }
+        else if(QueryObj.DegreeName == "0") // query with bio only
+        {
+            if(UserBio.includes(QueryObj.word))
+            {
+                ReturnObj.push(BachData);
+            }
+        }
+        else // query with both bio and degree
+        {
+            if(UserBio.includes(QueryObj.word))
+            {
+                var UserFound = 0;
+                for(var j=0; j<UserDeg.length && UserFound==0; j++)
+                {
+                    if(UserDeg[j].DegreeName == QueryObj.DegreeName && UserDeg[j].SalEtmam == QueryObj.SalEtmam)
+                    {
+                        UserFound = 1;
+                    }
+                }
+                if(UserFound==1)
+                {
+                    ReturnObj.push(BachData);
+                }
+            }
+        }
+    }
+    return ReturnObj;
+}
+
 var init = function () {
     NodesDir = arguments[0];
     if (!fs.existsSync(NodesDir)){
@@ -61,4 +168,8 @@ var init = function () {
 module.exports.submitedUserExists = submitedUserExists;
 module.exports.submitedUserConfirmed = submitedUserConfirmed;
 module.exports.submitUser = submitUser;
+module.exports.confirmedUserExists = confirmedUserExists;
+module.exports.confirmUser = confirmUser;
+module.exports.GetUserInfo = GetUserInfo;
+module.exports.SearchUserByBioWord = SearchUserByBioWord;
 module.exports.init = init;
--- index.js
+++ index.js
@@ -20,11 +20,22 @@ var OptionValues = ["کارشناسی", "کارشناسی ارشد", "دکترا
 app.get('/', function (req, res) {
     if(req.query.reqid)
     {
-        res.render('index', { });
+        res.render('index', { options : OptionValues });
     }
     else
     {
         res.redirect("/?reqid=564");
+    }
+});
+
+app.get('/uni', function (req, res) {
+    if(req.query.reqid)
+    {
+        res.render('uni', { options : OptionValues });
+    }
+    else
+    {
+        res.redirect("/uni?reqid=564");
     }
 });
 
@@ -88,6 +99,67 @@ app.post('/submitUser', function (req, res) {
             }
         }
     }
+});
+
+app.post('/confirmUser', function (req, res) {
+    console.log("confirmUser");
+    var IncomingData = JSON.parse(req.body.Data); // parsing incoming data in JSON
+    var DataComp = 1;
+    var TBachId = IncomingData.BachId,
+        TCodeMelli = IncomingData.CodeMelli,
+        TBachName = IncomingData.BachName,
+        TBachLname = IncomingData.BachLname,
+        TDegs = IncomingData.Degrees,
+        TDegsCount = IncomingData.DegreesCount;
+    if(TBachId == "") { DataComp = 0; }
+    if(TCodeMelli == "") { DataComp = 0; }
+    if(TBachName == "") { TBachName = "نام وارد نشده";}
+    if(TBachLname == "") { TBachLname = "نام خانوادگی وارد نشده"; }
+    if(TDegsCount==0) { DataComp = 0; }
+    if(DataComp==0)
+    {
+        res.send("");
+    }
+    else
+    {
+        if(DatabaseMS.confirmedUserExists(TBachId) == 1)
+        {
+            res.send(JSON.stringify({ message: "اطلاعات شما قبلا ثبت شده !!!" }));
+        }
+        else
+        {
+            var Bach = {
+                BachId: TBachId,
+                CodeMelli: TCodeMelli,
+                BachName: TBachName,
+                BachLname: TBachLname,
+                Degs: TDegs,
+                DegsCount: TDegsCount
+            };
+            DatabaseMS.confirmUser(TBachId, Bach);
+            res.send(JSON.stringify({ message: "اطلاعات شما با موفقیت ثبت شد..." }));
+        }
+    }
+});
+
+app.post('/searchUserById', function (req, res) {
+    console.log("searchUserById");
+    var IncomingData = JSON.parse(req.body.Data); // parsing incoming data in JSON
+    if(DatabaseMS.submitedUserExists(IncomingData.id)==1)
+    {
+        var foundUser = DatabaseMS.GetUserInfo(IncomingData.id);
+        res.render('foundUserById', foundUser);
+    }
+    else
+    {
+        res.send("هیچ دانشجویی با این شماره ثبت نشده است!!!");
+    }
+});
+
+app.post('/searchUserByBio', function (req, res) {
+    console.log("searchUserByBio");
+    var IncomingData = JSON.parse(req.body.Data); // parsing incoming data in JSON
+    res.render('foundUserByBio',{ Users: DatabaseMS.SearchUserByBioWord(IncomingData)});
 });
 
 app.listen(8000,function () {
